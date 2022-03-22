d51aaef2b90429cb31268a9ae3977ea9ca667489
Evandro Oliveira <evanoliv@unicamp.br>
2022-03-22 14:59:36 -0300

    teste

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
     public function teste()
     {
-        $user = Auth::user();
+        $user = User::where('email', Auth::user()->id);
 
         //$dados['usuarios'] = User::all();
         return view('usuarios.teste', [
