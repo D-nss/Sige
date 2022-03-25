445cf33d4ee33c709953dfc112210e6f016ed8c0
Evandro Oliveira <evanoliv@unicamp.br>
2022-03-25 11:12:50 -0300

    alteracoes_unidade

--- resources/views/indicadores/create.blade.php
+++ resources/views/indicadores/create.blade.php
@@ -6,7 +6,7 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-xl-12">
-            <h1>Adicionar Indicadores da unidade 38</h1>
+            <h1>Adicionar Indicadores da unidade - {{$unidade->sigla}}</h1>
 
             @if ($errors->any())
                 <div class="alert alert-danger">
@@ -22,9 +22,9 @@
             <form action="{{ url('/indicadores') }}" method="post">
             @csrf
                 <div id="smartwizard" class="sw-main sw-theme-default p-4">
-                    
-                @include('indicadores._form')                  
-                    
+
+                @include('indicadores._form')
+
                 </div>
             </form>
             </div>
