2400db7b840c08bf8392cb308c3af8c66c5191de
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-05-05 17:10:04 -0300

    Update modelo cronograma seeder

--- database/seeders/ModeloCronogramaTableSeeder.php
+++ database/seeders/ModeloCronogramaTableSeeder.php
@@ -56,12 +56,45 @@ class ModeloCronogramaTableSeeder extends Seeder
             array (
                 'id' => 4,
                 'tipo_edital' => 'PEX',
-                'dt_label' => 'Data Término dos projetos',
-                'dt_input' => 'dt_termino_projetos',
+                'dt_label' => 'Data Organização por Área temática',
+                'dt_input' => 'dt_org_tematica',
                 'validate' => 'dt_termino_inscricao',
-                'msg_erro' => 'A data de termino das inscrições não pode ser menor que a data de término das inscrições',
+                'msg_erro' => 'A data de organização por área temática não pode ser menor que a data  de término das inscrições',
                 'created_at' => '2022-04-12 14:08:54',
                 'updated_at' => '2022-04-12 14:08:54',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'tipo_edital' => 'PEX',
+                'dt_label' => 'Data Término Organização por Área Temática',
+                'dt_input' => 'dt_termino_org_tematica',
+                'validate' => 'dt_org_area_tematica',
+                'msg_erro' => 'A data  término de organização por área temática não poder ser menor que a  data inicial da organizção por área temática',
+                'created_at' => '2022-05-03 09:52:28',
+                'updated_at' => '2022-05-03 09:52:29',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'tipo_edital' => 'PEX',
+                'dt_label' => 'Data de Inicio de Análise dos Pareceristas',
+                'dt_input' => 'dt_pareceristas',
+                'validate' => 'dt_termino_org_area_tematica',
+                'msg_erro' => 'A data de Inicio da análise dos pareceristas não pode ser menor que a data de término da organização por área temática',
+                'created_at' => '2022-05-03 10:02:32',
+                'updated_at' => '2022-05-03 10:02:33',
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'tipo_edital' => 'PEX',
+                'dt_label' => 'Data Término de Análise dos Pareceristas',
+                'dt_input' => 'dt_termino_pareceristas',
+                'validate' => 'dt_pareceristas',
+                'msg_erro' => 'A data de término da análise dos pareceristas não pode ser menor que a data de término da data inicio de análise dos pareceristas',
+                'created_at' => '2022-05-03 10:02:34',
+                'updated_at' => '2022-05-03 10:02:34',
             ),
         ));
         
