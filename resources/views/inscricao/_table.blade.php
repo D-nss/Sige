866c8e50b6d096385764ce9e9dfab0f9ff0bcb2c
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-05-11 11:20:35 -0300

    Correção da exibição do nome do coordenador e da cidade na view show da inscricao

--- resources/views/inscricao/_table.blade.php
+++ resources/views/inscricao/_table.blade.php
@@ -10,17 +10,17 @@
     </thead>
     <tbody>
         @foreach($inscricoes as $inscricao)
-            @if( strtotime(date('Y-m-d')) >= strtotime($cronograma->getDate('dt_org_tematica', $inscricao->edital_id)))
+            @if( $user->hasAnyRole('super','edital-administrador') || strtotime(date('Y-m-d')) >= strtotime($cronograma->getDate('dt_org_tematica', $inscricao->edital_id)))
             <tr>
                 <td>{{ $inscricao->titulo }}</td>
                 <td>{{ $inscricao->user->name}}</td>
                 <td>{{ $inscricao->status }}</td>
                 <td>
                     <a href='{{ url("storage/$inscricao->anexo_projeto") }}' target="_blank" class="btn btn-danger btn-sm m-1">Ver PDF</a>
-                    @if($user->hasRole('analista|super') && strtotime(date('Y-m-d')) >= strtotime($cronograma->getDate('dt_org_tematica', $inscricao->edital_id)) && strtotime(date('Y-m-d')) <= strtotime($cronograma->getDate('dt_termino_org_tematica', $inscricao->edital_id)))
+                    @if($user->hasAnyRole('edital-analista','super','edital-administrador') && strtotime(date('Y-m-d')) >= strtotime($cronograma->getDate('dt_org_tematica', $inscricao->edital_id)) && strtotime(date('Y-m-d')) <= strtotime($cronograma->getDate('dt_termino_org_tematica', $inscricao->edital_id)))
                         <a href='{{ url("inscricao/$inscricao->id/?analise=true") }}' class="btn btn-warning btn-sm m-1">Análise</a>
                     @endif
-                    @if($user->hasRole('avaliador|super') && $inscricao->status == 'Deferido' && strtotime(date('Y-m-d')) >= strtotime($cronograma->getDate('dt_pareceristas', $inscricao->edital_id)) && strtotime(date('Y-m-d')) <= strtotime($cronograma->getDate('dt_termino_pareceristas', $inscricao->edital_id)))
+                    @if($user->hasAnyRole('edital-avaliador','super','edital-administrador') && $inscricao->status == 'Deferido' && strtotime(date('Y-m-d')) >= strtotime($cronograma->getDate('dt_pareceristas', $inscricao->edital_id)) && strtotime(date('Y-m-d')) <= strtotime($cronograma->getDate('dt_termino_pareceristas', $inscricao->edital_id)))
                         <a href='{{ url("inscricao/$inscricao->id/?avaliacao=true") }}' class="btn btn-success btn-sm m-1">Avaliar</a>
                     @endif
                 </td>
