a0c5b9395b764a3b2a9055a814280b0538c24a3c
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-05-17 11:21:29 -0300

    Correções de escrita

--- resources/views/processo-edital/index.blade.php
+++ resources/views/processo-edital/index.blade.php
@@ -4,10 +4,12 @@
 
 @section('content')
 <h1>Processos de Editais</h1>
+<h2>Crie e gerencie seus editais</h2>
 <div class="row mb-3">
     <div class="col-sm-3">
         <a href="{{ route('editais.create') }}" class="btn btn-success btn-lg btn-icon rounded-circle" >
             <i class="far fa-plus"></i>
+            Novo processo de edital
         </a>
     </div>
 </div>
@@ -16,7 +18,7 @@
 
 <div class="row">
     @if( isset( $editais ) )
-    @foreach( $editais as $edital)
+    @forelse( $editais as $edital)
     <div class="card border m-3 col-md-4 shadow" style="max-width: 18rem;">
         <img src='{{ !!$edital->anexo_imagem ? asset("storage/$edital->anexo_imagem" ) : asset("/smartadmin-4.5.1/img/pdf-icon.png") }}' class="card-img-top mt-3" alt="{{ $edital->titulo }}">
         <div class="card-body">
@@ -26,7 +28,9 @@
             <a href='{{ url("processo-editais/$edital->id/editar") }}' class="btn btn-info my-1 font-weight-bold"><i class="far fa-edit"></i> Editar</a>
         </div>
     </div>
-    @endforeach
+    @empty
+        <h3>Não há nenhum processo de edital cadastrado</h3>
+    @endforelse
     @endif
 </div>
 
