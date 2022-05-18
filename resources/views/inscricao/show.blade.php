bf52a90d5096cb175d56df1675fbb229a13fffc8
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-05-18 13:24:49 -0300

    Formatando valor monetário

--- resources/views/inscricao/show.blade.php
+++ resources/views/inscricao/show.blade.php
@@ -142,7 +142,7 @@
                                         <td>{{ $item->tipoitem }}</td>
                                         <td>{{ $item->descricao}}</td>
                                         <td>{{ $item->justificativa}}</td>
-                                        <td>R$ {{ $item->valor }}</td>
+                                        <td>R$ {{ number_format($item->valor, 2, ',', '.') }}</td>
                                     </tr>
                                 @endforeach
                                 </tbody>
