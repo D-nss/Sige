e4480ab9a8e51d508ade32ac6fbf7c2fe2f2c70f
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-05-25 15:03:17 -0300

    Update views

--- resources/views/processo-edital/create.blade.php
+++ resources/views/processo-edital/create.blade.php
@@ -3,20 +3,32 @@
 @section('title', 'Cadastrar novo Processo Edital')
 
 @section('content')
-<h1>Novo Processo de Edital</h1>
-        <!-- Begin Page Content -->
-        <div class="container">
-          <div class="row">
+<div class="container-fluid">
 
-            <div class="col-lg-12">
+    @include('layouts._includes._status')
 
-              @include('layouts._includes._status')
-              <!-- Default Card Example -->
-              @include('processo-edital._panel')
-
+    <div class="subheader">
+        <h1 class="subheader-title">
+            <span class="text-success">Processos de Editais</span>
+            <small>
+                Criar novo processo de edital
+            </small>
+        </h1>
+        <div class="subheader-block d-lg-flex align-items-center">
+            <div class="d-inline-flex flex-column justify-content-center">
+              
             </div>
-          </div>
         </div>
+    </div>
+    <div class="row">
+
+      <div class="col-lg-12">
+        <!-- Default Card Example -->
+        @include('processo-edital._panel')
+
+      </div>
+    </div>
+  </div>
         <!-- /.container-fluid -->
 
 @endsection
--- resources/views/processo-edital/index.blade.php
+++ resources/views/processo-edital/index.blade.php
@@ -3,37 +3,53 @@
 @section('title', 'Editais')
 
 @section('content')
-<h1>Processos de Editais</h1>
-<h3 class="font-color-light">Crie e gerencie seus editais</h3>
-<div class="row mb-3">
-    <div class="col-sm-3">
-        <a href="{{ route('editais.create') }}" class="btn btn-success btn-lg btn-icon rounded-circle" >
-            <i class="far fa-plus"></i>
-        </a>
-        Novo processo de edital
-    </div>
-</div>
+<div class="container-fluid">
 
-@include('layouts._includes._status')
+    @include('layouts._includes._status')
 
-<div class="row">
-    @if( isset( $editais ) )
-    @forelse( $editais as $edital)
-    <div class="card border m-3 col-md-4 shadow" style="max-width: 18rem;">
-        <img src='{{ !!$edital->anexo_imagem ? asset("storage/$edital->anexo_imagem" ) : asset("/smartadmin-4.5.1/img/pdf-icon.png") }}' class="card-img-top mt-3" alt="{{ $edital->titulo }}">
-        <div class="card-body">
-            <h2 class="card-text font-weight-bold">{{ $edital->titulo }}</h2>
-            <p><strong>Data de cadastro: </strong>{{ date('d/m/Y', strtotime($edital->created_at)) }}</p>
-            <p class="">{{ substr($edital->resumo, 0, 120) . ' ... ' }}</p>
-            <a href='{{ url("processo-editais/$edital->id/editar") }}' class="btn btn-info my-1 font-weight-bold"><i class="far fa-edit"></i> Editar</a>
+    <div class="subheader">
+        <h1 class="subheader-title">
+            <span class="text-success">Processos de Editais</span>
+            <small>
+                Crie e gerencie seus editais
+            </small>
+        </h1>
+        <div class="subheader-block d-lg-flex align-items-center">
+            <div class="d-inline-flex flex-column justify-content-center">
+                
+            </div>
         </div>
     </div>
-    @empty
-    <div class="col-md-6">
-        <h4 class="font-color-light">Não há nenhum processo de edital cadastrado</h4>
+
+    <div class="row">
+        <div class="col-xl-12">
+            <a href="{{ route('editais.create') }}" class="btn btn-success btn-lg btn-icon rounded-circle" >
+                <i class="far fa-plus"></i>
+            </a>
+            Novo processo de edital
+        </div>
     </div>
-    @endforelse
-    @endif
+
+    <div class="row">
+        @if( isset( $editais ) )
+        @forelse( $editais as $edital)
+        <div class="card border m-3 col-md-4 shadow" style="max-width: 18rem;">
+            <img src='{{ !!$edital->anexo_imagem ? asset("storage/$edital->anexo_imagem" ) : asset("/smartadmin-4.5.1/img/pdf-icon.png") }}' class="card-img-top mt-3" alt="{{ $edital->titulo }}">
+            <div class="card-body">
+                <h2 class="card-text font-weight-bold">{{ $edital->titulo }}</h2>
+                <p><strong>Data de cadastro: </strong>{{ date('d/m/Y', strtotime($edital->created_at)) }}</p>
+                <p class="">{{ substr($edital->resumo, 0, 120) . ' ... ' }}</p>
+                <a href='{{ url("processo-editais/$edital->id/editar") }}' class="btn btn-info my-1 font-weight-bold"><i class="far fa-edit"></i> Editar</a>
+            </div>
+        </div>
+        @empty
+        <div class="col-md-6">
+            <h4 class="font-color-light">Não há nenhum processo de edital cadastrado</h4>
+        </div>
+        @endforelse
+        @endif
+    </div>
+
 </div>
 
 @endsection
