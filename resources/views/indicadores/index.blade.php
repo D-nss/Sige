ca152a6a59bbbcc117efa404282282366401b9c8
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-05-25 14:47:08 -0300

    Update views

--- resources/views/indicadores/index.blade.php
+++ resources/views/indicadores/index.blade.php
@@ -16,22 +16,24 @@
         </h1>
         <div class="subheader-block d-lg-flex align-items-center">
             <div class="d-inline-flex flex-column justify-content-center">
-            Adicionar Indicadores <a class="btn btn-success btn-lg btn-icon rounded-circle" href="{{ url('/indicadores/novo') }}"><i class="far fa-plus"></i></a>
+            
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-xl-12">
-        
-            @foreach($indicadores as $indicador)
-            <div class="p-3 mb-5 bg-white hv-light-green rounded d-flex justify-content-between">
-                <h4>{{ $indicador->ano_base }}</h4>
-                <div>
-                <a class="btn btn-secondary" href="{{ url('/indicadores/' . $indicador->ano_base . '/editar') }}">Editar</a>
-                <a class="btn btn-primary" href="{{ url('/indicadores/' . $indicador->ano_base) }}">Ver</a>
+            <a class="btn btn-success btn-lg btn-icon rounded-circle" href="{{ url('/indicadores/novo') }}"><i class="far fa-plus"></i></a>Adicionar Indicadores
+            <div class="mt-3">
+                @foreach($indicadores as $indicador)
+                <div class="p-3 mb-5 bg-white hv-light-green rounded d-flex justify-content-between">
+                    <h4>{{ $indicador->ano_base }}</h4>
+                    <div>
+                    <a class="btn btn-warning" href="{{ url('/indicadores/' . $indicador->ano_base . '/editar') }}">Editar</a>
+                    <a class="btn btn-info" href="{{ url('/indicadores/' . $indicador->ano_base) }}">Ver</a>
+                    </div>
                 </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
 </div>
