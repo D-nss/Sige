bacee1ef9ab4c7035284297d7f39e02e32e7a3ff
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-06-01 14:47:50 -0300

    Add botão e metodo de submissão de inscricão

--- resources/views/inscricao/enviadas.blade.php
+++ resources/views/inscricao/enviadas.blade.php
@@ -48,12 +48,37 @@
                                 <a href='{{ url("inscricao/$inscricao->id/editar") }}' class="btn btn-info m-1"><i class="far fa-edit"></i> Editar</a>
                                 <a href='{{ url("inscricao/$inscricao->id/orcamento") }}' class="btn btn-primary m-1"><i class="far fa-list"></i> Orçamento</a>
                                 <!-- Button trigger modal -->
-                                <button type="button" class="btn btn-success m-1" data-toggle="modal" data-target="#{{$inscricao->id}}Modal">
-                                    <i class="far fa-arrow-right"></i> Submeter
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                Launch demo modal
                                 </button>
 
                                 <!-- Modal -->
-                                <div class="modal fade" id="{{$inscricao->id}}Modal" tabindex="-1" aria-labelledby="{{$inscricao->id}}ModalLabel" aria-hidden="true">
+                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                                <!-- Button trigger modal -->
+                                <!-- <button type="button" class="btn btn-success m-1" data-toggle="modal" data-target="#{{$inscricao->id}}Modal">
+                                    <i class="far fa-arrow-right"></i> Submeter
+                                </button> -->
+
+                                <!-- Modal -->
+                                <!-- <div class="modal fade" id="{{$inscricao->id}}Modal" tabindex="-1" aria-labelledby="{{$inscricao->id}}ModalLabel" aria-hidden="true">
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                         <div class="modal-header">
@@ -77,7 +102,7 @@
                                         </div>
                                         </div>
                                     </div>
-                                </div>
+                                </div> -->
                                 
                                 @endif
                             </td>
