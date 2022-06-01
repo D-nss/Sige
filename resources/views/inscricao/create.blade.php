5e818a67295e60dfe9becdc4760e12e575f525c6
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-06-01 10:14:19 -0300

    Correcao de validação de inscricao

--- resources/views/inscricao/create.blade.php
+++ resources/views/inscricao/create.blade.php
@@ -134,7 +134,6 @@
                                                 </div>
                                                 <div class="box-body" id="comprovante-box-body">
                                                     @if( old('parceria') == 'Sim' )
-                                                    {{ dd(old('pdf_projeto')) }}
                                                         <img src='{{ url("smartadmin-4.5.1/img/pdf-icon.png") }}' alt="Comprovante" class="img-thumbnail mb-2" style="max-width: 75px;" />    
                                                     @endif
 
