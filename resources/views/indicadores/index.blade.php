34e2b7fea27fe745b65e2128d76d304c58b5705a
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-06-06 14:20:30 -0300

    Limitar edição indicadores com base na data limite

--- resources/views/indicadores/index.blade.php
+++ resources/views/indicadores/index.blade.php
@@ -26,12 +26,12 @@
             <div class="mt-3">
                 @forelse($indicadores as $indicador)
                 <div class="p-3 mb-5 bg-white hv-light-green rounded d-flex justify-content-between">
-                    <h4>{{ $indicador->ano_base }}</h4>
+                    <h4>{{ $indicador['ano_base'] }}</h4>
                     <div>
-                    
+                        @if( strtotime( date('Y-m-d') ) <= strtotime($indicador['data_limite']) )
                         <a class="btn btn-warning" href="{{ url('/indicadores/' . $indicador->ano_base . '/editar') }}">Editar <i class="far fa-edit"></i></a>
-                   
-                    <a class="btn btn-info" href="{{ url('/indicadores/' . $indicador->ano_base) }}">Ver <i class="far fa-eye"></i></a>
+                        @endif
+                        <a class="btn btn-info" href="{{ url('/indicadores/' . $indicador->ano_base) }}">Ver <i class="far fa-eye"></i></a>
                     </div>
                 </div>
                 @empty
