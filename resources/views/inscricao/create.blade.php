91b88cfc70988306971d93ff0e1d5eb2aa62251e
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-06-09 09:13:25 -0300

    Alteração da quantidade de caracter para resumo e detalhamento na inscrição

--- resources/views/inscricao/create.blade.php
+++ resources/views/inscricao/create.blade.php
@@ -233,7 +233,9 @@
                                     <h3 class="text-success">Áreas Temáticas</h3>
                                     <h5>Pressione a tecla Ctrl para poder selecionar mais de uma opção</h5>
                                     <select name="areas_tematicas[]" class="form-control mb-3" style="height: 150px;" multiple required>
+                                    @if( isser( old('areas_tematicas') ) )
                                     {{ dd( old('areas_tematicas') )}}
+                                    @endif
                                         @foreach($areas_tematicas as $area_tematica)
                                             <option value="{{ $area_tematica->id }}" @if(old('areas_tematicas') == '{{ $area_tematica->id }}' || (isset($inscricao->areas) && $inscricao->areas->contains($area_tematica->id))) selected @endif>{{ $area_tematica->nome }}</option>
                                         @endforeach
