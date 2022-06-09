7f3c83092054bda63dfd18b893c9e06f69954385
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-06-09 09:22:54 -0300

    Alteração da quantidade de caracter para resumo e detalhamento na inscrição

--- resources/views/inscricao/create.blade.php
+++ resources/views/inscricao/create.blade.php
@@ -233,11 +233,11 @@
                                     <h3 class="text-success">Áreas Temáticas</h3>
                                     <h5>Pressione a tecla Ctrl para poder selecionar mais de uma opção</h5>
                                     <select name="areas_tematicas[]" class="form-control mb-3" style="height: 150px;" multiple required>
-                                    @if(  old('areas_tematicas') !== null)
-                                    {{ dd( old('areas_tematicas') )}}
+                                    @if(  old('areas_tematicas[]') !== null)
+                                    {{ dd( old('areas_tematicas[]') )}}
                                     @endif
                                         @foreach($areas_tematicas as $area_tematica)
-                                            <option value="{{ $area_tematica->id }}" @if(old('areas_tematicas') == '{{ $area_tematica->id }}' || (isset($inscricao->areas) && $inscricao->areas->contains($area_tematica->id))) selected @endif>{{ $area_tematica->nome }}</option>
+                                            <option value="{{ $area_tematica->id }}" @if(old('areas_tematicas[]') == '{{ $area_tematica->id }}' || (isset($inscricao->areas) && $inscricao->areas->contains($area_tematica->id))) selected @endif>{{ $area_tematica->nome }}</option>
                                         @endforeach
                                             <!-- <option value="2" @if(old('areas_tematicas') == '2') selected @endif>Cultura</option>
                                         <option value="3" @if(old('areas_tematicas') == '3') selected @endif>Direitos Humanos e Justiça</option>
