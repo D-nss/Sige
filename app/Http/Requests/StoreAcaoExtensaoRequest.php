453d03b806361efcd7148837523e443822dad5a3
Evandro Oliveira <evanoliv@unicamp.br>
2022-06-22 17:23:28 -0300

    evanoliv

--- app/Http/Requests/StoreAcaoExtensaoRequest.php
+++ app/Http/Requests/StoreAcaoExtensaoRequest.php
@@ -13,7 +13,7 @@ class StoreAcaoExtensaoRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- resources/views/acoes-extensao/create.blade.php
+++ resources/views/acoes-extensao/create.blade.php
@@ -8,20 +8,31 @@
     <h1>Cadastrar nova Ação de Extensão</h1>
   <div class="row">
     <div class="col-xl-12">
-        <div class="panel">
-            <form action="{{route('acao_extensao.store')}}" id="form_acao_extensao" method="POST">
-                @csrf
-                @include('acoes-extensao._form')
-                <a href="/acoes-extensao" class="btn btn-secondary btn-user btn-block ">
-                    <span class="icon text-white-50">
-                    <i class="fas fa-arrow-left"></i>
-                    </span>
-                    <span class="text">Voltar</span>
-                </a>
-            </form>
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    Formulário <span class="fw-300"><i>Insira as informações nos campos correspondentes</i></span>
+                </h2>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
+                    <div class="panel-tag">
+                        Espaço para informar os erros na validação do formulário.
+                    </div>
+                    <form action="{{route('acao_extensao.store')}}" id="form_acao_extensao" method="POST">
+                        @csrf
+                        @include('acoes-extensao._form')
+                        <a href="/acoes-extensao" class="btn btn-secondary btn-user btn-block ">
+                            <span class="icon text-white-50">
+                            <i class="fas fa-arrow-left"></i>
+                            </span>
+                            <span class="text">Voltar</span>
+                        </a>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-
   </div>
 </div>
 
