3e1b0d8d1cbbcf4694031cff4ee8ed4cba6020bc
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-06-27 16:51:51 -0300

    Add schedule que notifica inscrições com orçamento faltante quando a data de inscrição está se encerrando

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
                 }
             }
 
-        })->dailyAt('16:20')->timezone('America/Fortaleza');
+        })->dailyAt('00:01')->timezone('America/Fortaleza');
 
         $schedule->call(function(){
             $users = \App\Models\User::all();
