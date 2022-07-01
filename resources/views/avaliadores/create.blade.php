04a987d275ea7d597cdae1b8bc7c8e1f4ecc85a1
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-07-01 16:05:33 -0300

    Add header nas viwes indicadores, inscricao e editais

--- resources/views/avaliadores/create.blade.php
+++ resources/views/avaliadores/create.blade.php
@@ -25,19 +25,7 @@
 </div>
 
 <div class="container-fluid">
-    <div class="subheader">
-        <h1 class="subheader-title">
-            <span class="text-success">Avaliadores</span>
-            <small>
-            Cadastrar os avaliadores para o edital <span class="text-secondary">{{$edital->titulo}}</span>
-            </small>
-        </h1>
-        <div class="subheader-block d-lg-flex align-items-center">
-            <div class="d-inline-flex flex-column justify-content-center">
-            
-            </div>
-        </div>
-    </div>
+    
     <div class="row">
       <div class="col-lg-12">
         <div class="card mb-4">
