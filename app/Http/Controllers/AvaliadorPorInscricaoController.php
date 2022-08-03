c3e8f5c92bda3a2b1e6e48222d918ebdc0ec7c7e
Andre Moreira <aadilson@DESKTOP-H0BL7M2.ad.proec.unicamp.br>
2022-08-03 11:27:24 -0300

    Correções de layout e avaliação das inscrições

--- app/Http/Controllers/AvaliadorPorInscricaoController.php
+++ app/Http/Controllers/AvaliadorPorInscricaoController.php
@@ -10,7 +10,7 @@ class AvaliadorPorInscricaoController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('role:edital-administrador|super');
+        $this->middleware('role:edital-analista|edital-administrador|super');
     }
     
     /**
