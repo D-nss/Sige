cecffea13ce21ede8e648bbb0fc77644a9b7428f
André Moreira <aadilson@unicamp.br>
2022-08-19 13:50:17 -0300

    Correções no form da inscrição

--- resources/views/inscricao/create.blade.php
+++ resources/views/inscricao/create.blade.php
@@ -300,7 +300,7 @@
         <!-- /.container-fluid -->
         <!-- Modal center -->
         <div class="modal fade" id="aviso-modal" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h4 class="modal-title">
