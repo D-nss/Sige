e212a6f0b3ee5b5effe57d61ed95b3755a120af7
André Moreira <aadilson@unicamp.br>
2022-08-24 13:45:18 -0300

    Checagem de exibiçãod o botão parecer

--- resources/views/inscricao/_botoes_pex.blade.php
+++ resources/views/inscricao/_botoes_pex.blade.php
@@ -1,4 +1,5 @@
 <div class="">
+{{ dd($inscricao->avaliadores) }}
     <a href='{{ url("inscricao/$inscricao->id") }}' class="btn btn-info btn-xs m-1"><i class="far fa-eye"></i> Ver</a>
     <a href='{{ url("storage/$inscricao->anexo_projeto") }}' target="_blank" class="btn btn-danger btn-xs m-1"><i class="far fa-pdf"></i> Ver PDF</a>
     
@@ -20,6 +21,4 @@
             <a href='{{ url("inscricao/$inscricao->id/indicar-analista") }}' class="btn btn-secondary btn-xs m-1">Indicar Analista</a>
         @endif
     @endif
-
-    {{ dd($inscricao->avaliadores) }}
 </div>
