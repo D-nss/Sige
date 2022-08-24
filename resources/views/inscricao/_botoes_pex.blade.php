78df2cd0cb4fbf34405200cdf2667cf9fecb6870
André Moreira <aadilson@unicamp.br>
2022-08-24 13:53:28 -0300

    Checagem de exibiçãod o botão parecer

--- resources/views/inscricao/_botoes_pex.blade.php
+++ resources/views/inscricao/_botoes_pex.blade.php
@@ -12,7 +12,7 @@
         @endif
     @endif
     @if($user->hasAnyRole('super','edital-administrador') || ( $inscricao->status == 'Deferido' && strtotime(date('Y-m-d')) >= strtotime($cronograma->getDate('dt_pareceristas', $inscricao->edital_id)) && strtotime(date('Y-m-d')) <= strtotime($cronograma->getDate('dt_termino_pareceristas', $inscricao->edital_id))))
-        @if($inscricao->respostas_avaliacoes->where('id', '==', $user->id)->count() == 0 && $inscricao->status == 'Deferido')
+        @if($inscricao->respostas_avaliacoes->where('id', '==', $user->id)->count() == 0 && $inscricao->avaliadores->where('id', '==', $user->id)->count() > 0)
             <a href='{{ url("inscricao/$inscricao->id/?tipo_avaliacao=parecerista") }}' class="btn btn-success btn-xs m-1">Parecer</a>
         @endif
     @endif
