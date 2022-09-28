3bfe88a9964eae67b5e695288ac308fa9ab64d6f
root <root@extecult.csfbcloud.internal>
2022-09-28 13:09:22 -0300

    UserController from server extecult

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
         //$this->middleware('permission:user-create', ['only' => ['create','store']]);
         //$this->middleware('permission:user-edit', ['only' => ['edit','update']]);
         //$this->middleware('permission:user-delete', ['only' => ['destroy']]);
-        $this->middleware('role:super');
+        $this->middleware('role:super', ['except' => ['teste']]);
     }
 
     public function teste()
