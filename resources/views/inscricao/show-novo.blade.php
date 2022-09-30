c872cbae4dc188e3ba6554d5605d5bc2629d3472
André Moreira <aadilson@unicamp.br>
2022-09-30 13:17:58 -0300

    Add checagem se tem nota na inscrição para exibir o resultado

--- resources/views/inscricao/show-novo.blade.php
+++ resources/views/inscricao/show-novo.blade.php
@@ -56,7 +56,7 @@
                                             
             <div class="frame-wrap w-100">
                 <div class="accordion" id="accordionExample">
-                @if( (strtotime(date('Y-m-d')) >= strtotime($cronograma->getDate('dt_divulgacao_previa', $inscricao->edital_id))) )
+                @if( (strtotime(date('Y-m-d')) >= strtotime($cronograma->getDate('dt_divulgacao_previa', $inscricao->edital_id)))  && $inscricao->nota != null)
                     <div class="card">
                         <div class="card-header bg-success bg-primary-gradient" id="headingFour">
                             <a href="javascript:void(0);" class="card-title collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
