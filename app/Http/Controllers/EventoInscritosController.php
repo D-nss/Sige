806cd1415ecc1576799e24f5911f4b99a3a94d3f
André Moreira <aadilson@unicamp.br>
2022-12-13 13:26:22 -0300

    Corrigindo cancelamento de inscrição

--- app/Http/Controllers/EventoInscritosController.php
+++ app/Http/Controllers/EventoInscritosController.php
@@ -98,32 +98,26 @@ class EventoInscritosController extends Controller
 
             return redirect()->back();
         }
+
+        if($inscrito && $data[0] == 'nao')
+        {
+            $inscrito->confirmacao = 2;
+            if($inscrito->update()) {
+                return view('eventos.inscritos.confirmacao', compact('inscrito'));
+            }
+            else {
+                session()->flash('status', 'Desculpe! Houve um erro ao realizar a confirmação inscrição.');
+                session()->flash('alert', 'danger');
+
+                return redirect()->back();
+            }
+        }
+        else {
+            session()->flash('status', 'Desculpe! Não foi possível confirmação inscrição.');
+            session()->flash('alert', 'danger');
+
+            return redirect()->back();
+        }
     }
 
-    public function cancelar($codigo)
-    {
-        $decrypt = \Illuminate\Support\Facades\Crypt::decryptString(str_replace('90', '09', $codigo));
-        $data = explode('/', $decrypt);
-        echo json_encode($data);
-        // $inscrito = EventoInscrito::find($data[1]);
-        // if($inscrito && $data[0] == 'nao')
-        // {
-        //     $inscrito->confirmacao = 2;
-        //     if($inscrito->update()) {
-        //         return view('eventos.inscritos.confirmacao', compact('inscrito'));
-        //     }
-        //     else {
-        //         session()->flash('status', 'Desculpe! Houve um erro ao realizar a confirmação inscrição.');
-        //         session()->flash('alert', 'danger');
-
-        //         return redirect()->back();
-        //     }
-        // }
-        // else {
-        //     session()->flash('status', 'Desculpe! Não foi possível confirmação inscrição.');
-        //     session()->flash('alert', 'danger');
-
-        //     return redirect()->back();
-        // }
-    }
 }
