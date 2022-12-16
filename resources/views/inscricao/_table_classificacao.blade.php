a1c117e8853c4b1b2122ab011ccb1aeadb860423
André Moreira <aadilson@unicamp.br>
2022-12-16 10:02:59 -0300

    Add valor orcado na datatable dos classificados

--- resources/views/inscricao/_table_classificacao.blade.php
+++ resources/views/inscricao/_table_classificacao.blade.php
@@ -6,6 +6,7 @@
             <th>Tipo</th>
             <th>Coordenador</th>
             <th>Nota Final</th>
+            <th>Valor Solicitado</th>
         </tr>
     </thead>
     <tbody>
@@ -17,7 +18,7 @@
                 <td><h6 class="text-secondary">{{ $inscricao->tipo}}</h6></td>
                 <td><h6 class="text-secondary">{{ $inscricao->user->name}}</h6><small class="font-italic font-color-light">Unidade: {{ $inscricao->unidade->sigla}}</small></td>
                 <td><span class="badge badge-success badge-pill">{{ $inscricao->nota }}</span></td>
-               
+                <td>R$ {{ number_format($inscricao->orcamento->sum('valor'), 2, ',', '.') }}</td>
             </tr>
             
         @endforeach
