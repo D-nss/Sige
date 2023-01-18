adc8640b6a2d7c76964cf251adf50a84cad9ed38
André Moreira <aadilson@unicamp.br>
2023-01-18 15:28:52 -0300

    Corrigindo o title das paginas de dados da inscrição

--- resources/views/inscricao/show-novo.blade.php
+++ resources/views/inscricao/show-novo.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Cadastrar novo Processo Edital')
+@section('title', 'Dados da Inscrição')
 
 @section('content')
 <ol class="breadcrumb page-breadcrumb">
