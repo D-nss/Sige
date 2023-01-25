6413eb8c141b7fcdd1c00bede460c6f4109cb459
André Moreira <aadilson@unicamp.br>
2023-01-25 09:59:29 -0300

    Debug na criação de comissão

--- resources/views/inscricao/_table_classificacao.blade.php
+++ resources/views/inscricao/_table_classificacao.blade.php
@@ -3,6 +3,7 @@
         <tr>
             <th>Edital</th>
             <th>Inscrição</th>
+            <th>Linha de Extenção</th>
             <th>Tipo</th>
             <th>Coordenador</th>
             <th>Unidade</th>
@@ -15,7 +16,8 @@
         @foreach($inscricoes as $inscricao)
             <tr>
                 <td><h5 class="fw-400 text-secondary">{{ $inscricao->edital->titulo }}</h5></td>
-                <td><h3 class="fw-700 text-primary">{{ $inscricao->titulo }}</h3><small class="font-italic font-color-light">Linhas de Extensão: {{ $inscricao->linha_extensao->nome}}</small></td>
+                <td><h3 class="fw-700 text-primary">{{ $inscricao->titulo }}</h3></td>
+                <td><small class="font-italic font-color-light">{{ $inscricao->linha_extensao->nome}}</small></td>
                 <td><h6 class="text-secondary">{{ $inscricao->tipo}}</h6></td>
                 <td><h6 class="text-secondary">{{ $inscricao->user->name}}</h6></td>
                 <td><h6 class="font-italic font-color-light">{{ $inscricao->unidade->sigla}}</h6></td>
