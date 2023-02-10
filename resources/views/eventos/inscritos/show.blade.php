1374966b6b75054fcde1a68bdf7bbe969145abea
André Moreira <aadilson@unicamp.br>
2023-02-10 11:25:02 -0300

    Removendo erro na view show do inscrito de evento

--- resources/views/eventos/inscritos/show.blade.php
+++ resources/views/eventos/inscritos/show.blade.php
@@ -295,7 +295,7 @@
                                                             <form action="" method="POST">
                                                                 <div class="modal-content">
                                                                 <div class="modal-header">
-                                                                    <h5 class="modal-title" id="exampleModalLabel{{ $comissao->id }}">Analisar Arquivo</h5>
+                                                                    <h5 class="modal-title" id="exampleModalLabel{{ $inscrito->id }}">Analisar Arquivo</h5>
                                                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                                     <span aria-hidden="true">&times;</span>
                                                                     </button>
