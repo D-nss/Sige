63911cf5e16a9b364815477986e172abf3d64e0b
André Moreira <aadilson@unicamp.br>
2023-02-14 14:49:04 -0300

    Melhorias no design

--- resources/views/eventos/inscritos/confirmacao.blade.php
+++ resources/views/eventos/inscritos/confirmacao.blade.php
@@ -37,9 +37,9 @@
                         </div>
                     </div>
                     <div class="flex-1 ml-4">
-                        <span class="h6 font-weight-bold text-uppercase d-block m-0">{{ $inscrito->nome }} sua inscrição está confirmada no evento!</span>
-                        <span class="fs-sm text-secundary h6 fw-500 mb-0 d-block">Salve um print do QRCode abaixo ou clique no botão baixar</span>
-                        <span class="fs-sm text-secundary h6 fw-500 mb-0 d-block">O acesso ao evento se dará através deste QRCode, por isso é importante apresenta-lo na portaria do evento</span>
+                        <span class="h6 font-weight-bold text-uppercase d-block m-0 mb-3">{{ $inscrito->nome }} sua inscrição está confirmada no evento!</span>
+                        <span class="fs-sm text-secundary h6 fw-500 m-0 mb-3 d-block">Salve um print do QRCode abaixo ou clique no botão baixar</span>
+                        <span class="fs-sm text-secundary h6 fw-500 m-0 mb-3 d-block">O acesso ao evento se dará através deste QRCode, por isso é importante apresenta-lo na portaria do evento</span>
                         <p>Para acompanhamento de inscrição e envio de arquivo acesse sua área clicando no botão abaixo.</p>
                         <a href="{{ url('evento/inscrito/' . $inscrito->id) }}" class="btn btn-success">Área do Inscrito</a>
                     </div>
