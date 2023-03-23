1c59e6077f2271a0596a5ec24b5df942ac6bc516
André Moreira <aadilson@unicamp.br>
2023-03-23 14:58:59 -0300

    Corrigindo notificação da abertura de novas vagas do evento (Módulo Eventos)

--- app/Notifications/EventoAberturaVagaInscritoNotificar.php
+++ app/Notifications/EventoAberturaVagaInscritoNotificar.php
@@ -7,6 +7,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
+use App\Models\Evento;
+
 class EventoAberturaVagaInscritoNotificar extends Notification
 {
     use Queueable;
