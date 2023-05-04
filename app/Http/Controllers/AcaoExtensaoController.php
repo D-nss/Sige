78f1107b5102e55c14bd0a92e73ac29d3a081954
André Moreira <aadilson@unicamp.br>
2023-05-04 10:22:50 -0300

    Corrigindo notificação da aprovação local (Modulo Ações de Extensão)

--- app/Http/Controllers/AcaoExtensaoController.php
+++ app/Http/Controllers/AcaoExtensaoController.php
@@ -737,7 +737,7 @@ class AcaoExtensaoController extends Controller
                                     ->where('comissoes.atribuicao', 'Conext')
                                     ->get('users.*');
         // Notificando a comissao conext 
-        Notification::send($users, new AcaoExtensaoAprovadaComissaoNotificar($acaoExtensao));
+        Notification::send($comissaoConext, new AcaoExtensaoAprovadaComissaoNotificar($acaoExtensao));
         session()->flash('status', 'Ação de Extensão aprovada!');
         session()->flash('alert', 'success');
 
