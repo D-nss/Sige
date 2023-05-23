d4dd44d397f6bd3992d9e182944febf32f677fb8
Evandro Oliveira <evanoliv@unicamp.br>
2023-05-23 11:32:23 -0300

    Criacao inicial Ocorrencias para Ação Extensão

---
+++ app/Models/AcaoExtensaoOcorrencia.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AcaoExtensaoOcorrencia extends Model
+{
+    use HasFactory;
+
+    protected $table = 'acoes_extensao_ocorrencias';
+
+    protected $dates = ['data_hora_inicio', 'data_hora_fim'];
+
+    protected $fillable = [
+        'acao_extensao_id',
+        'data_hora_inicio',
+        'data_hora_fim',
+        'local',
+        'complemento',
+        'latitude',
+        'longitude',
+        'inicio_inscricoes',
+        'fim_inscricoes'
+    ];
+
+    public function acao_extensao()
+    {
+        return $this->belongsTo(AcaoExtensao::class);
+    }
+}
---
+++ database/migrations/2023_05_23_111748_create_acao_extensao_ocorrencias_table.php
@@ -0,0 +1,40 @@
+<?php
+use App\Models\AcaoExtensao;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('acao_extensao_ocorrencias', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(AcaoExtensao::class);
+            $table->string('local', 255);
+            $table->dateTime('data_hora_inicio');
+            $table->dateTime('data_hora_fim');
+            $table->dateTime('inicio_inscricoes');
+            $table->dateTime('fim_inscricoes');
+            $table->string('complemento', 255)->nullable();
+            $table->string('latitude', 255)->nullable();
+            $table->string('longitude', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('acao_extensao_ocorrencias');
+    }
+};
