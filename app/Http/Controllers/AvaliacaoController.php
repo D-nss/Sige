93c34e9b1042775159f0bbc575808d1f53b740a7
André Moreira <aadilson@unicamp.br>
2023-05-30 10:31:30 -0300

    Debug avaliação do parecerista (Modulo Inscricao Editais)

--- app/Http/Controllers/AvaliacaoController.php
+++ app/Http/Controllers/AvaliacaoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\AvaliadorPorInscricao;
 use App\Models\User;
 use App\Models\Inscricao;
 use App\Models\RespostasAvaliacoes;
