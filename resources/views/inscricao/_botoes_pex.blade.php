910a077d3f6229f725a3522597f8e036614d19e5
André Moreira <aadilson@unicamp.br>
2023-07-28 14:45:19 -0300

    Corrigindo exibição das listagem das inscrições para analise do relatorio final (Modulo Editais)

--- resources/views/inscricao/_botoes_pex.blade.php
+++ resources/views/inscricao/_botoes_pex.blade.php
@@ -1,15 +1,14 @@
 <div class="">
     @if($user->hasAnyRole('super','edital-administrador'))
         <div class="d-flex flex-column">
-            
+            <a href='{{ url("inscricao/show-completo/$inscricao->id") }}' class="btn btn-info btn-xs m-1">
+                <i class="far fa-eye"></i> 
+                Visualização completa
+            </a>
+            @if($inscricao->status == 'Relatório em Análise')
+                <span class="fw-300 color-dark-500"><i class="fal fa-exclamation-circle"></i><i> Análise pendente! Acesse aqui.</i></span>
+            @endif
         </div>
-        <a href='{{ url("inscricao/show-completo/$inscricao->id") }}' class="btn btn-info btn-xs m-1">
-            <i class="far fa-eye"></i> 
-            Visualização completa
-        </a>
-        @if($inscricao->status == 'Relatório em Análise')
-            <span class="fw-300 color-dark-500"><i class="fal fa-exclamation-circle"></i><i> Análise pendente! Acesse aqui.</i></span>
-        @endif
     @else
         <a href='{{ url("inscricao/$inscricao->id") }}' class="btn btn-info btn-xs m-1"><i class="far fa-eye"></i> Ver</a>
     @endif
