89879bb05664b5b746b4ea5c83d6cb19ba828e35
Evandro Oliveira <evanoliv@unicamp.br>
2023-10-25 17:01:22 -0300

    Ajustes views Módulo Eventos

--- resources/views/eventos/inscritos/_inscritos_comissao.blade.php
+++ resources/views/eventos/inscritos/_inscritos_comissao.blade.php
@@ -6,11 +6,11 @@
                     <a href="javascript:void(0);" class="card-title" data-toggle="collapse" data-target="#collapseOne"
                         aria-expanded="true" aria-controls="collapseOne">
                         <div class='icon-stack display-3 flex-shrink-0'>
-                            <i class="fal fa-circle icon-stack-3x opacity-100 color-info-400"></i>
-                            <i class="far fa-users icon-stack-1x opacity-100 color-info-500"></i>
+                            <i class="fal fa-circle icon-stack-3x opacity-100 color-primary"></i>
+                            <i class="far fa-check icon-stack-1x opacity-100 color-primary"></i>
 
                         </div>
-                        <h4 class="ml-2 mb-0 flex-1 text-info fw-500">
+                        <h4 class="ml-2 mb-0 flex-1 fw-500">
                             Inscritos Confirmados <span class="alert-info p-1">Avaliação</span>
                         </h4>
                         <span class="ml-auto">
