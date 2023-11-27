aca1904ee60fd812ed79d6ab38d86600042ab2b7
Evandro Oliveira <evanoliv@unicamp.br>
2023-11-27 11:52:13 -0300

    Correção view show membro equipe

--- resources/views/eventos/equipe/show.blade.php
+++ resources/views/eventos/equipe/show.blade.php
@@ -121,9 +121,28 @@
                                             </div>
                                         @else
                                             @if($membroEquipe->funcao_evento == 'Staff')
-                                                {{ $evento->carga_horaria }}
+                                            <div class="col-12">
+                                                <div class="p-0">
+                                                    <h5>
+                                                        <span class="font-color-light font-size-14">Carga horária</span>
+                                                        <small class="mt-0 mb-3 font-size-16 fw-400 text-uppercase">
+                                                            {{ $evento->carga_horaria }} hora(s)
+                                                        </small>
+                                                    </h5>
+                                                </div>
+                                            </div>
                                             @else
-                                                <a href="{{ url('evento/' . $evento->id . '/equipe/' . $memembroEquipe->id . '/editar') }}">Inserir horas</a>
+                                            <div class="col-12">
+                                                    <div class="p-0">
+                                                        <h5>
+                                                            <span class="font-color-light font-size-14">Carga horária</span>
+                                                            <small class="mt-0 mb-3 font-size-16 fw-400 text-uppercase">
+                                                                <a href="{{ url('evento/' . $evento->id . '/equipe/' . $membroEquipe->id . '/editar') }}">Inserir horas</a>
+                                                            </small>
+                                                        </h5>
+                                                    </div>
+                                                </div>
+
                                             @endif
                                         @endif
                                         <a href="{{ url('evento/' . $evento->id . '/equipe/' . $membroEquipe->id . '/certificado') }}"
