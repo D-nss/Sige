4858759cc9f34722e7cfd1b0447c69614d269950
André Moreira <aadilson@unicamp.br>
2023-12-16 10:48:43 -0300

    Correção erro ao incluir recurso (Modulo Editais)

--- app/Http/Controllers/Editais/RecursoInscricaoController.php
+++ app/Http/Controllers/Editais/RecursoInscricaoController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Log;
 
 use App\Models\Inscricao;
 use App\Models\Recurso;
