fce41c61da187ae74e36e18e81b3993bc8a96540
André Moreira <aadilson@unicamp.br>
2024-01-02 16:59:22 -0300

    Corrigindo checagem de comissão de graduação (Modulo Acoes de Extensao)

--- app/Services/Comissao/BuscaUsuariosComissaoGraduacao.php
+++ app/Services/Comissao/BuscaUsuariosComissaoGraduacao.php
@@ -17,7 +17,7 @@ class BuscaUsuariosComissaoGraduacao
     {
         $usersComissao = User::join('comissoes_users as cmu', 'cmu.user_id', 'users.id')
                                     ->join('comissoes as c', 'c.id', 'cmu.comissao_id')
-                                    ->where('c.unidade_id', $unidade->id)
+                                    ->where('c.unidade_id', $unidade)
                                     ->where('c.atribuicao', 'Graduação')
                                     ->get('users.*');
         
