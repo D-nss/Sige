1b63870e4b045099f2860e7f865105ab4d5240ba
André Moreira <aadilson@unicamp.br>
2024-01-23 09:23:34 -0300

    Corrigindo o update da ação de extensao (Modulo Acoes de Extensao)

--- app/Http/Requests/UpdateAcaoExtensaoRequest.php
+++ app/Http/Requests/UpdateAcaoExtensaoRequest.php
@@ -35,7 +35,8 @@ class UpdateAcaoExtensaoRequest extends FormRequest
             'estado' => 'required',
             'cidade' => 'required',
             'impactos_universidade' => 'required|max:2500',
-            'impactos_sociedade' => 'required|max:2500'
+            'impactos_sociedade' => 'required|max:2500',
+            'arquivo' => 'required|file|max:5120|mimes:pdf',
         ];
     }
 }
