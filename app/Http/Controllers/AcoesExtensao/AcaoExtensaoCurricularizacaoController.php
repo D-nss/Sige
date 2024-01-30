ca1b550a5e06e70a441d27f2b83102ee9ed354fc
André Moreira <aadilson@unicamp.br>
2024-01-30 11:33:39 -0300

    Ajustando curricularizacao para  usar tabela refactoring 03(Modulo acoes de extensao)

--- app/Http/Controllers/AcoesExtensao/AcaoExtensaoCurricularizacaoController.php
+++ app/Http/Controllers/AcoesExtensao/AcaoExtensaoCurricularizacaoController.php
@@ -60,22 +60,18 @@ class AcaoExtensaoCurricularizacaoController extends Controller
         $matricula = Auth::user()->matricula;
 
         $dadosAluno = Dbsig::where('NREGALUN', $matricula)->get();
-       
-        if($dadosAluno[0]->SITALUNO != "Regular - Ativo") {
+        
+        if($dadosAluno[0]->SITALUNO == "Regular - Ativo" || $dadosAluno[0]->SITALUNO == "Especial - Ativo") {
+            return view('acoes-extensao.curricularizacao.create', compact('acao_extensao_ocorrencia', 'dadosAluno'));
+        }
+        else {
             session()->flash('status', 'Desculpe! Somente alunos regulares e ativos podem participar da curricularização.');
             session()->flash('alert', 'warning');
 
             return redirect()->back();
         }
 
-        if($dadosAluno[0]->SITALUNO != "Especial - Ativo") {
-            session()->flash('status', 'Desculpe! Somente alunos regulares e ativos podem participar da curricularização.');
-            session()->flash('alert', 'warning');
-
-            return redirect()->back();
-        }
-
-        return view('acoes-extensao.curricularizacao.create', compact('acao_extensao_ocorrencia', 'dadosAluno'));
+        
     }
 
     public function store(Request $request, AcaoExtensaoOcorrencia $acao_extensao_ocorrencia)
