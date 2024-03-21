0c53e40f4021e7d08014df4167e72d9f008c1670
André Moreira <andreinformatica@terra.com.br>
2024-03-21 15:43:35 -0300

    Redesigne do cadastro de comissoes 0.06 dump(Modulo acoes de extensao)

--- resources/views/acoes-extensao/inicio.blade.php
+++ resources/views/acoes-extensao/inicio.blade.php
@@ -59,7 +59,7 @@
             </div>
         </div>
     </div>
-    {{ dd($comissao_extensao) }}
+    {{ dd($comissao_extensao->users) }}
     <div class="modal fade" id="modalComissaoExtensao" tabindex="-1" aria-labelledby="modalLabelComissaoExtensao" aria-hidden="true">
         <div class="modal-dialog">
                 <div class="modal-content">
