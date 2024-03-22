88f4067d0a8c510b8de7ee7822d1a24f0b2dc727
André Moreira <andreinformatica@terra.com.br>
2024-03-22 14:16:36 -0300

    Redesign do cadastro de comissoes 0.23 debug (Modulo acoes de extensao)

--- app/Http/Controllers/AcoesExtensao/AcaoExtensaoController.php
+++ app/Http/Controllers/AcoesExtensao/AcaoExtensaoController.php
@@ -61,7 +61,7 @@ class AcaoExtensaoController extends Controller
         $comissao_graduacao = Comissao::where('unidade_id', $user->unidade_id)
             ->where('atribuicao', 'Graduação')->first();
 
-        $users = User::orderBy('name', 'asc')->all();
+        $users = User::orderBy('name', 'asc')->get();
 
 
         return view('acoes-extensao.inicio', [
