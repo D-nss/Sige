c59f9723536d526bd98153a8fb0f66a5f86622c1
André Moreira <andreinformatica@terra.com.br>
2024-03-26 16:30:59 -0300

    Redesign do cadastro de comissoes 0.25 (Modulo acoes de extensao)

---
+++ app/Http/Controllers/AcoesExtensao/ComissaoAcaoController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\AcoesExtensao;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Comissao;
+use App\Models\ComissaoUser;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class ComissaoAcaoController extends Controller
+{
+     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAcaoExtensaoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $transacao = DB::transaction(function() use ($request){
+            $comissao = Comissao::create(
+                [
+                    'nome' => $request->nome_comissao,
+                    'atribuicao'    => "Extensão",
+                    'edital_id'     => null,
+                    'unidade_id'    => Auth::user()->unidade->id,
+                    'evento_id'     => null,
+                ]
+            );
+
+            if($comissao) {
+                Log::channel('comissoes')->info('Usuario Nome: ' . Auth::user()->name . ' - Usuario ID: ' . Auth::user()->id . ' - Info: Comissao ID: '. $comissao->id .' criada com sucesso - Endereço IP: ' . $request->ip());
+            }
+            else{
+                Log::channel('comissoes')->error('Usuario Nome: ' . Auth::user()->name . ' - Usuario ID: ' . Auth::user()->id . ' - Erro: Comissao NÃO criada - Endereço IP: ' . $request->ip());
+            }
+
+            foreach($request->membros as $membro) {
+
+                $comissao_user = ComissaoUser::create([
+                    'user_id' => $membro['id'],
+                    'comissao_id' => $comissao->id,
+                ]);
+
+                if($comissao_user) {
+                    Log::channel('comissoes')->info('Usuario Nome: ' . Auth::user()->name . ' - Usuario ID: ' . Auth::user()->id . ' - Info: Usuário ID: '. $membro['nome'] .' adicionado a comissao ID '.$membro['id'].' - Endereço IP: ' . $request->ip());
+                    $comissao->user->notify(new \App\Notifications\ComissaoUserAdicionado($comissao_user));
+
+                }
+                else {
+                    Log::channel('comissoes')->error('Usuario Nome: ' . Auth::user()->name . ' - Usuario ID: ' . Auth::user()->id . ' - Info: Usuário ID: '. $membro['nome'] .' não adicionado a comissao ID '.$membro['id'].' - Endereço IP: ' . $request->ip());
+                 
+                }
+            }
+
+        });
+
+        if(is_null($transacao) || empty($transacao)) {
+            $session = [
+                'status' => 'Inscrição atualizada com sucesso.',
+                'alert' => 'success'
+            ];           
+        }
+        else {
+            $session = [
+                'status' => 'Desculpe! Houve erro ao criar a comissão',
+                'alert' =>  'danger'
+            ];
+        }
+        
+        echo json_encode($session);
+    }
+}
--- resources/views/acoes-extensao/inicio.blade.php
+++ resources/views/acoes-extensao/inicio.blade.php
@@ -86,24 +86,20 @@
                     <form action="" method="post">
                         <div class="form-group">
                             <h3>Nome da Comissão de Extensão</h3>
-                            <input type="text" name="nome_comissao" id="nome_comissao" class="form-control" placeholder="Digite aqui. Letras, números e caracteres especiais são permitidos." @if($comissao_extensao)value="{{ $comissao_extensao->nome}}" disabled @endif>
+                            <input type="text" name="nome_comissao" id="nome_comissao" class="form-control" placeholder="Digite aqui. Letras, números e caracteres especiais são permitidos.">
                         </div>
                         <div class="form-group">
                             <h3>Adicionar membros</h3>
-                            <select name="nome_membro[]" id="nome_membro" multiple class="form-control" @if($comissao_extensao) disabled @endif>
-                                <option value="">Selecione da lista ...</option>
+                            <select name="nome_membro" id="nome_membro" class="form-control" >
+                                <option value="" disabled>Selecione da lista ...</option>
                                 @foreach($users as $user)
                                     <option value="{{ $user->id }}">{{ $user->name }} - {{ $user->unidade->sigla }}</option>
                                 @endforeach
                             </select>
                         </div>
                         <span>Nomes selecionados</span>
-                        <div class="border rounded p-2" style="min-height: 80px">
-                            @if($comissao_extensao)
-                                @foreach($comissao_extensao->users as $membro)
-                                    <div class="badge badge-primary badge-pill p-2" style="background: #4679CC !important">{{ $membro->name }}</div>
-                                @endforeach
-                            @endif
+                        <div class="border rounded p-2" style="min-height: 120px" id="nomes_selecionados">
+                            
                         </div>
                         <p class="mt-3">
                             O próximo passo será a criação de uma Comissão de Graduação, que responderá às solicitações de curricularização.
@@ -111,7 +107,7 @@
                         <div class="form-group mt-2">
                             <div class="row">
                                 <div class="col">
-                                <button type="submit" class="btn @if($comissao_extensao) btn-secondary @else btn-primary @endif" @if($comissao_extensao) disabled @endif>
+                                <button type="button" class="btn btn-secondary" disabled id="btn-criar-comissao">
                                     <i class="far fa-check-circle"></i>
                                     Criar
                                 </button>
@@ -127,6 +123,81 @@
                             </div>                            
                         </div>
                     </form>
+                      
+
+                </div>
+                <!-- <div class="modal-footer">
+                    
+                </div> -->
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalComissaoGraducao" tabindex="-1" aria-labelledby="modalLabelComissaoGraduacao" aria-hidden="true">
+        <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalLabelComissaoGraduacao">
+                        Criar Comissão de Graduação - Unidade: {{ $user->unidade->sigla }}
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>
+                    Cadastrar uma <span class="fw-700">Comissão de Graduação</span> 
+                    </p>
+
+                    <h3>
+                        
+                    </h3>
+
+                    <p>
+                   
+                    </p>
+                    
+                    <!-- <form action="" method="post">
+                        <div class="form-group">
+                            <h3>Nome da Comissão de Extensão</h3>
+                            <input type="text" name="nome_comissao" id="nome_comissao" class="form-control" placeholder="Digite aqui. Letras, números e caracteres especiais são permitidos.">
+                        </div>
+                        <div class="form-group">
+                            <h3>Adicionar membros</h3>
+                            <select name="nome_membro" id="nome_membro" class="form-control" >
+                                <option value="" disabled>Selecione da lista ...</option>
+                                @foreach($users as $user)
+                                    <option value="{{ $user->id }}">{{ $user->name }} - {{ $user->unidade->sigla }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <span>Nomes selecionados</span>
+                        <div class="border rounded p-2" style="min-height: 120px" id="nomes_selecionados">
+                            
+                        </div>
+                        <p class="mt-3">
+                            O próximo passo será a criação de uma Comissão de Graduação, que responderá às solicitações de curricularização.
+                        </p>
+                        <div class="form-group mt-2">
+                            <div class="row">
+                                <div class="col">
+                                <button type="button" class="btn btn-secondary" disabled id="btn-criar-comissao">
+                                    <i class="far fa-check-circle"></i>
+                                    Criar
+                                </button>
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col"> 
+                                    <button type="button" class="btn btn-primary" data-dismiss="modal">
+                                        <i class="far fa-times"></i>
+                                        Cancelar
+                                    </button>
+                                </div>                            
+                            </div>                            
+                        </div>
+                    </form> -->
                       
 
                 </div>
