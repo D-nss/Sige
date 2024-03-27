ab5222c2eb5f231addb7ac118d3906df766577a5
André Moreira <andreinformatica@terra.com.br>
2024-03-27 15:01:00 -0300

    Redesign do cadastro de comissoes 0.43 (Modulo acoes de extensao)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1793,7 +1793,9 @@ Versão: 4.5.1
                 });
                 //fim da criacao da comissao no inicio do modulo de acoes de extensao
                 
-                
+                @if(isset($comissao_extensao) && $comissao_extensao && isset($comissao_graduacao) && !$comissao_graduacao)
+                    $("#modalComissaoGraduacao").modal('show');
+                @endif
             @endif
             /* Usando API para auxiar no preenchimento do endereço com latitude e longitude
             Mas apresenta erros - por enquanto desconsiderar...
