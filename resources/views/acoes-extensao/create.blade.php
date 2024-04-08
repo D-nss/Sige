4110813e7e561266304c4b0401446e4db4a93bd7
André Moreira <aadilson@unicamp.br>
2024-04-08 09:40:34 -0300

    Redesign Proposta de acao debug 1(Modulo de ações de extensao)

--- resources/views/acoes-extensao/create.blade.php
+++ resources/views/acoes-extensao/create.blade.php
@@ -81,6 +81,7 @@
                     </div>
                     <div class="row">
                         <div class="col-lg-12">
+                            @dd($user->hasRole('extensao-coordenador'))
                             @if($user->hasRole('extensao-coordenador') && !$comissao_graduacao)
                                 <!-- coordenador mas sem comissao de graduacao -->
                                 <div class="alert alert-warning">
