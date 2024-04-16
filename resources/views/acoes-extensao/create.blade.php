bccac5d5c94cad640b3b19203fe8b4ebc26dbe7d
André Moreira <aadilson@unicamp.br>
2024-04-16 09:04:49 -0300

    Aplicando a barra de status da ação debug 4 (Modulo de ações de extensao)

--- resources/views/acoes-extensao/create.blade.php
+++ resources/views/acoes-extensao/create.blade.php
@@ -33,43 +33,7 @@
                         </p>
                     </div>
                     <div class="row border-top mb-5 pt-3">
-                        <div class="col-lg-5">
-                            <h2>Situação Atual</h2>
-                            <div class=" p-3 bg-secondary rounded" style="height: 80px">
-                                <div class="row text-light justify-content-center align-items-center">
-                                    <div class="d-flex justify-content-center align-items-center mr-2">
-                                        <div class="d-flex flex-column justify-content-center align-items-center mr-2">
-                                            <div class="mb-2">
-                                            <div class="bg-warning rounded-circle" style="width:27px; height:27px"></div>
-                                            </div>
-                                            
-                                            <small class="text-center">Proposta</small>
-                                        </div>
-                                        <span class="text-warning"><i class="far fa-angle-right fa-2x"></i></span>
-                                    </div>
-                                    <div class="d-flex justify-content-center align-items-center mr-2">
-                                        <div class="d-flex flex-column justify-content-center align-items-center mr-2">
-                                            <div class="bg-light rounded-circle" style="width:27px; height:27px"></div>
-                                            <small class="text-center">Registro<br>Unidade</small>
-                                        </div>
-                                        <span class="text-light"><i class="far fa-angle-right fa-2x"></i></span>
-                                    </div>
-                                    <div class="d-flex justify-content-center align-items-center mr-2">
-                                        <div class="d-flex flex-column justify-content-center align-items-center mr-2">
-                                            <div class="bg-light rounded-circle" style="width:27px; height:27px"></div>
-                                            <small class="text-center">Reconheci<br>mento ProEC</small>
-                                        </div>
-                                        <span class="text-light"><i class="far fa-angle-right fa-2x"></i></span>
-                                    </div>
-                                    <div class="d-flex justify-content-center align-items-center mr-2">
-                                        <div class="d-flex flex-column justify-content-center align-items-center mr-2">
-                                            <div class="bg-light rounded-circle" style="width:27px; height:27px"></div>
-                                            <small class="text-center">Ação<br>Publicada!</small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @include('acoes-extensao.statusbar')
                         <div class="col-lg-7">
                             <h2>Próximos Passos</h2>
                             <div class="p-3 bg-secondary rounded d-flex justify-content-center align-items-center" style="height: 80px">
