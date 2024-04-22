da93f55bda9876aa8aa9528cc6bddfbe7e45561d
André Moreira <aadilson@unicamp.br>
2024-04-22 09:46:06 -0300

    Ajustes em textos(Modulo de ações de extensao)

--- resources/views/layouts/_includes/sidebar.blade.php
+++ resources/views/layouts/_includes/sidebar.blade.php
@@ -190,11 +190,20 @@
                         </a>
                         <ul>
                             <li class="">
+                                <a href="{{ url('acoes-extensao/inicio') }}" title="Inicio"
+                                    data-filter-tags="utilities menu child sublevel item">
+                                    <span class="nav-link-text"
+                                        data-i18n="nav.utilities_menu_child_sublevel_item">Inicio</span>
+                                </a>
+                            </li>
+                            <li class="">
                                 <a href="{{ url('acoes-extensao-ocorrencias/catalogo') }}" title="Inscrições Abertas"
                                     data-filter-tags="utilities menu child sublevel item">
                                     <span class="nav-link-text"
                                         data-i18n="nav.utilities_menu_child_sublevel_item">Inscrições Abertas</span>
                                 </a>
+                            </li>
+                            <li class="">
                                 <a href="{{ url('acoes-extensao-participacoes') }}" title="Participações"
                                     data-filter-tags="utilities menu child sublevel item">
                                     <span class="nav-link-text"
